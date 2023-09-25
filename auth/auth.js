9e7121dc40f393e685efe5f2f559a0b1bfb38c18
Michaelrand45223 <michaelrandriam45@gmail.com>
2023-09-25 23:49:10 +0200

    s'inscrire et se connecter

---
+++ auth/auth.js
@@ -0,0 +1,88 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5XUFZdpnniSJh7rfo8nJ-C6RopWTsNF0",
+    authDomain: "rhapsodie-25374.firebaseapp.com",
+    projectId: "rhapsodie-25374",
+    storageBucket: "rhapsodie-25374.appspot.com",
+    messagingSenderId: "339820898811",
+    appId: "1:339820898811:web:ad07ee153fbb5c5fe2435e"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+
+const auth = app.auth()
+
+// Gestion de l'inscription d'un utilisateur
+function signUp(email, password) {
+    console.log("inscription")
+    auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // L'utilisateur est inscrit avec succès
+            var user = userCredential.user;
+            // Faites quelque chose avec l'utilisateur, par exemple, redirigez-le vers une page sécurisée.
+        })
+        .catch((error) => {
+            // Gestion des erreurs d'inscription
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+}
+
+// Gestion de la connexion d'un utilisateur
+function signIn(email, password) {
+    console.log("connexion")
+    auth.signInWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+            // L'utilisateur est connecté avec succès
+            var user = userCredential.user;
+            // Faites quelque chose avec l'utilisateur, par exemple, redirigez-le vers une page sécurisée.
+        })
+        .catch((error) => {
+            // Gestion des erreurs de connexion
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
+}
+
+// Gestion de la déconnexion de l'utilisateur
+function signOut() {
+    auth.signOut().then(() => {
+        // L'utilisateur est déconnecté avec succès
+    }).catch((error) => {
+        // Gestion des erreurs de déconnexion
+    });
+}
+
+const inscription = (e) => {
+    console.log("connexion");
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    console.log({ email, password });
+    if (password.length < 6) {
+        alert("Veuillez mettre un mot de passe avec plus de 6 caractères");
+    }
+    // TODO: chercher un validateur email regexp
+    else {
+        signUp(email, password);
+    }
+};
+
+
+const login = (e) => {
+    console.log("connexion");
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    console.log({ email, password });
+    // TODO: chercher un validateur email regexp*
+    try {
+        signIn(email, password);
+    }
+    catch (e) {
+        console.log({ e })
+    }
+};
