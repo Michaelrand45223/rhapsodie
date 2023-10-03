ee0f9e518022c5473b531b1ef7af9686bcb3c44f
Michaelrand45223 <132446393+Michaelrand45223@users.noreply.github.com>
2023-10-03 12:40:51 +0200

    Update Administrateur.js

--- Administrateur.js
+++ Administrateur.js
@@ -1,36 +1,25 @@
-// Données fictives des salariés
-const employees = [
-    { name: "Alice", absences: 2, daysWorked: 20, sickLeaves: 1, paidLeaves: 1 },
-    { name: "Bob", absences: 0, daysWorked: 22, sickLeaves: 0, paidLeaves: 2 },
-    // Ajoutez plus de salariés ici
-];
+// administrateur.js
 
-// Fonction pour afficher les données dans le tableau
-function displayEmployees() {
-    const table = document.getElementById("employeeTable");
+// Fonction pour récupérer les informations de l'administrateur
+function getAdminInfo() {
+    // Utilisez Firebase pour récupérer les informations de l'administrateur
+    const adminEmail = "admin@example.com"; // Remplacez par l'email de l'administrateur
 
-    for (const employee of employees) {
-        const row = table.insertRow(-1);
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-        const cell3 = row.insertCell(2);
-        const cell4 = row.insertCell(3);
-        const cell5 = row.insertCell(4);
-        const cell6 = row.insertCell(5);
-
-        cell1.innerHTML = employee.name;
-        cell2.innerHTML = employee.absences;
-        cell3.innerHTML = employee.daysWorked;
-        cell4.innerHTML = employee.sickLeaves;
-        cell5.innerHTML = employee.paidLeaves;
-        cell6.innerHTML = `<button onclick="validate('${employee.name}')">Valider</button>`;
-    }
+    db.collection("users").where("email", "==", adminEmail).get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const adminData = doc.data();
+                // Affichez les informations sur la page
+                document.getElementById("admin-firstname").textContent = adminData.firstname;
+                document.getElementById("admin-lastname").textContent = adminData.name;
+                document.getElementById("admin-role").textContent = adminData.role;
+                document.getElementById("admin-site").textContent = adminData.site;
+            });
+        })
+        .catch((error) => {
+            console.error("Erreur lors de la récupération des informations de l'administrateur : ", error);
+        });
 }
 
-// Fonction pour valider les données d'un salarié
-function validate(name) {
-    alert(`Les données de ${name} ont été validées.`);
-}
-
-// Afficher les données lorsque la page est chargée
-window.onload = displayEmployees;
+// Appelez la fonction pour obtenir les informations de l'administrateur lors du chargement de la page
+getAdminInfo();
