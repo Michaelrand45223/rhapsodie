c2bbbf24bf8d62ea46875e1ee3468a184b8a91fb
Michaelrand45223 <132446393+Michaelrand45223@users.noreply.github.com>
2023-10-03 14:15:21 +0200

    Update Administrateur.js

--- Administrateur.js
+++ Administrateur.js
@@ -1,25 +1,46 @@
-// administrateur.js
+ <!-- Script pour récupérer et afficher les informations de l'utilisateur -->
+   <script src="https://www.gstatic.com/firebasejs/10.4.0/firebase-app-compat.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore-compat.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/10.4.0/firebase-auth-compat.js"></script>
+    <script>
+        const firebaseConfig = {
+            apiKey: "AIzaSyD5XUFZdpnniSJh7rfo8nJ-C6RopWTsNF0",
+    authDomain: "rhapsodie-25374.firebaseapp.com",
+    projectId: "rhapsodie-25374",
+    storageBucket: "rhapsodie-25374.appspot.com",
+    messagingSenderId: "339820898811",
+    appId: "1:339820898811:web:ad07ee153fbb5c5fe2435e"
+        };
 
-// Fonction pour récupérer les informations de l'administrateur
-function getAdminInfo() {
-    // Utilisez Firebase pour récupérer les informations de l'administrateur
-    const adminEmail = "admin@example.com"; // Remplacez par l'email de l'administrateur
+        firebase.initializeApp(firebaseConfig);
+        const auth = firebase.auth();
+        const db = firebase.firestore();
 
-    db.collection("users").where("email", "==", adminEmail).get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                const adminData = doc.data();
-                // Affichez les informations sur la page
-                document.getElementById("admin-firstname").textContent = adminData.firstname;
-                document.getElementById("admin-lastname").textContent = adminData.name;
-                document.getElementById("admin-role").textContent = adminData.role;
-                document.getElementById("admin-site").textContent = adminData.site;
-            });
-        })
-        .catch((error) => {
-            console.error("Erreur lors de la récupération des informations de l'administrateur : ", error);
+        // Fonction pour récupérer les informations de l'utilisateur actuellement connecté
+        auth.onAuthStateChanged((user) => {
+            if (user) {
+                // Utilisateur connecté
+                const email = user.email;
+
+                // Récupérez les informations de l'utilisateur à partir de la base de données
+                db.collection("users")
+                    .where("email", "==", email)
+                    .get()
+                    .then((querySnapshot) => {
+                        querySnapshot.forEach((doc) => {
+                            const data = doc.data();
+                            document.getElementById("firstName").textContent = data.firstname;
+                            document.getElementById("lastName").textContent = data.name;
+                            document.getElementById("userRole").textContent = data.role;
+                            document.getElementById("userSite").textContent = data.site;
+                        });
+                    })
+                    .catch((error) => {
+                        console.error("Erreur lors de la récupération des informations de l'utilisateur:", error);
+                    });
+            } else {
+                // Utilisateur non connecté, redirigez-le vers la page de connexion
+                window.location.href = "connexion.html"; // 
+            }
         });
-}
-
-// Appelez la fonction pour obtenir les informations de l'administrateur lors du chargement de la page
-getAdminInfo();
+    </script>
